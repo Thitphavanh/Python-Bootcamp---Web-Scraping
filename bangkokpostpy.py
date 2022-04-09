8831118613a93c6462f05fe81b029de60c744f18
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-09 14:31:08 +0700

    update

--- bangkokpostpy.py
+++ bangkokpostpy.py
@@ -3,22 +3,32 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url = 'https://www.bangkokpost.com/business/'
-# url = 'https://www.matichon.co.th/sport'
+def BangkokPost(cat='business'):
+    url = 'https://www.bangkokpost.com/{}/'.format(cat)
+    # url = 'https://www.matichon.co.th/sport'
 
-webopen = urlopen(url)
-pagehtml = webopen.read().decode('utf-8')
-webopen.close()
+    webopen = urlopen(url)
+    pagehtml = webopen.read().decode('utf-8')
+    webopen.close()
 
 
-data = BeautifulSoup(pagehtml, 'html.parser')
+    data = BeautifulSoup(pagehtml, 'html.parser')
 
-news_row = data.find_all('div', {'class':'listnews-text'})
-print(news_row[0].h3.a['href'])
+    news_row = data.find_all('div', {'class':'listnews-text'})
+    print(news_row[0].h3.a['href'])
 
-for n in news_row:
-    print(n.h3.text)
-    print(n.p.text)
-    print('https://www.bangkokpost.com' + n.h3.a['href'])
-    print('----------')
+    result = []
 
+    for n in news_row:
+        title = n.h3.text
+        detail = n.p.text
+        link = 'https://www.bangkokpost.com' + n.h3.a['href']
+        print(title)
+        print(detail)
+        print(link)
+        result.append([title, detail, link])
+        print('----------')
+
+    return result
+
+BangkokPost('tech')
