19dbb6ff31427808b6217c863f8dc6592b0b125e
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-09 11:41:36 +0700

    update

--- matichon.py
+++ matichon.py
@@ -3,8 +3,8 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-# url = 'https://www.matichon.co.th/bkkpataya/governorbkk'
-url = 'https://www.matichon.co.th/sport'
+url = 'https://www.matichon.co.th/bkkpataya/governorbkk'
+# url = 'https://www.matichon.co.th/sport'
 
 webopen = urlopen(url)
 pagehtml = webopen.read().decode('utf-8')
@@ -16,4 +16,10 @@ webopen.close()
 # print(pagehtml[start:end])
 
 data = BeautifulSoup(pagehtml,'html.parser')
-print([data.get_text().replace('\n',' ')])
+# print([data.get_text().replace('\n',' ')])
+
+title = data.find_all('h3',{'class':'entry-title td-module-title'})
+
+for t in title:
+	print(t.text)
+	print('------')
