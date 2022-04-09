fd5da1c26ddcde165af28fb2c153ccd3f93ce606
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-09 11:31:05 +0700

    update

--- matichon.py
+++ matichon.py
@@ -1,1 +1,19 @@
 # matichon.py
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# url = 'https://www.matichon.co.th/bkkpataya/governorbkk'
+url = 'https://www.matichon.co.th/sport'
+
+webopen = urlopen(url)
+pagehtml = webopen.read().decode('utf-8')
+
+webopen.close()
+
+# start = pagehtml.find('<title>') + len('<title>')
+# end = pagehtml.find('</title>')
+# print(pagehtml[start:end])
+
+data = BeautifulSoup(pagehtml,'html.parser')
+print([data.get_text().replace('\n',' ')])
