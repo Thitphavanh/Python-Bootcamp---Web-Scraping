00b69c60f718e17fa228964eef03961a2bf45110
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-10 12:20:33 +0700

    update

--- pptoil.py
+++ pptoil.py
@@ -21,29 +21,32 @@ driver = webdriver.Chrome(service=ser, options=opt)
 
 
 # url ເລີ່ມຕົ້ນ
-url = 'https://www.pttor.com/th/oil_prices/'
+url = 'https://www.pttor.com/th/oil_price/'
 driver.get(url)
 
 allresult = []
 
-for i in range(2, 5):
-    dropdown = driver.find_element(By.XPATH, '//*@id="__layout"]/div/div/section/div[2]/div/div/div[2]/form/div[2]/select/option[{}]')
+for i in range(2,5):
+    # range(2,5) ເລືອກ dropdown ຈາກ 2-5 ຕ້ອງກວດສອບ xpath
+    dropdown = driver.find_element(By.XPATH,'//*[@id="__layout"]/div/div/section/div[2]/div/div/div[2]/form/div[2]/select/option[{}]'.format(i))
     dropdown.click()
-    time.sleep(1)
-    table = driver.find_element(By.TAG_NAME, 'table')
-    row = table.find_elements(By.TAG_NAME, 'tr')
+    time.sleep(1) #ລໍຖ້າເວລາປະມວນຜົນ
+    table = driver.find_element(By.TAG_NAME,'table')
+    row = table.find_elements(By.TAG_NAME,'tr')
 
     for r in row:
-        column = r.find_elements(By.TAG_NAME, 'td')
+        column = r.find_elements(By.TAG_NAME,'td')
         day = []
         for c in column:
             # print(c.text)
             day.append(c.text)
-        print(day)
+        # print(day)
         if len(day) != 0:
             allresult.append(day)
-    # print(table.text)
-    print('-----------')
+
+    #print(table.text)
+    print('-------------')
 
 print(allresult)
+
 driver.close()
