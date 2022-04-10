790717cfd06ad0997f40549de84dc45208d12f0f
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-10 16:50:27 +0700

    update

---
+++ pantip.py
@@ -0,0 +1,60 @@
+# pantip.py
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from urllib.request import urlopen
+
+# ລະບຸຕຳແໜ່ງ webdriver
+# path = r'C:\Users\Hery\Desktop\Python Bootcamp 2022\WebScraping\chromedriver_win32\chromedriver.exe'
+# ser = Service(path)
+
+
+'''
+options = webdriver.ChromeOptions()
+# options.headless = True
+# options.add_argument("start-maximized")
+options.add_argument('--enable-javascript')
+
+# ສ້າງ driver
+driver = webdriver.Chrome(service=ser, options=options)
+
+url = 'https://pantip.com/forum/blueplanet'
+driver.get(url)
+'''
+
+# print(driver.page_source)
+# url = 'https://pantip.com/forum/blueplanet'
+url = 'https://pantip.com/forum/sinthorn'
+webopen = urlopen(url)
+html = webopen.read().decode('utf-8')
+webopen.close()
+
+# html = driver.page_source
+data = BeautifulSoup(html, 'html.parser')
+data = data.find('div', {'id': 'pt-topic-left'})
+forum = data.find_all(
+    'a', {'class': 'gtm-latest-topic gtm-topic-layout-compact gtm-topic-type-filter-all'})
+
+count = 1
+
+for f in forum:
+    if f.i != None:
+        print('No.', count)
+        print(f.text)
+        print(f['href'])
+        count += 1
+        detailurl = f['href']
+        webopen = urlopen(detailurl)
+        html = webopen.read().decode('utf-8')
+        webopen.close()
+        data = BeautifulSoup(html, 'html.parser')
+        detail = data.find('div', {'class': 'display-post-story'})
+        print('====')
+        print(detail.text.strip().replace('\xa0', ''))
+
+        print('-----')
+
+print('COUNT:', len(forum))
